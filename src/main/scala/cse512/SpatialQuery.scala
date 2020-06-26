e8e336839743722305ea9d8c2cd28a535ae8f2ca
rjunc <47236826+rjunc@users.noreply.github.com>
2020-06-25 19:58:19 -0700

    Update SpatialQuery.scala

--- src/main/scala/cse512/SpatialQuery.scala
+++ src/main/scala/cse512/SpatialQuery.scala
@@ -9,7 +9,7 @@ object SpatialQuery extends App{
     pointDf.createOrReplaceTempView("point")
 
     // YOU NEED TO FILL IN THIS USER DEFINED FUNCTION
-    spark.udf.register("ST_Contains", ST_Contains)
+    spark.udf.register("ST_Contains", (queryRectangle:String, pointString:String)=>((ST_Contains(queryRectangle, pointString))))
 
     val resultDf = spark.sql("select * from point where ST_Contains('"+arg2+"',point._c0)")
     resultDf.show()
@@ -26,7 +26,7 @@ object SpatialQuery extends App{
     rectangleDf.createOrReplaceTempView("rectangle")
 
     // YOU NEED TO FILL IN THIS USER DEFINED FUNCTION
-    spark.udf.register("ST_Contains", ST_Contains)
+    spark.udf.register("ST_Contains", (queryRectangle:String, pointString:String)=>((ST_Contains(queryRectangle, pointString))))
 
     val resultDf = spark.sql("select * from rectangle,point where ST_Contains(rectangle._c0,point._c0)")
     resultDf.show()
@@ -40,7 +40,7 @@ object SpatialQuery extends App{
     pointDf.createOrReplaceTempView("point")
 
     // YOU NEED TO FILL IN THIS USER DEFINED FUNCTION
-    spark.udf.register("ST_Within", ST_Within)
+    spark.udf.register("ST_Within", (pointString1:String, pointString2:String, distance:Double)=>((ST_Within(pointString1, pointString2, distance))))
 
     val resultDf = spark.sql("select * from point where ST_Within(point._c0,'"+arg2+"',"+arg3+")")
     resultDf.show()
@@ -57,52 +57,57 @@ object SpatialQuery extends App{
     pointDf2.createOrReplaceTempView("point2")
 
     // YOU NEED TO FILL IN THIS USER DEFINED FUNCTION
-    spark.udf.register("ST_Within",ST_Within)
+    spark.udf.register("ST_Within", (pointString1:String, pointString2:String, distance:Double)=>((ST_Within(pointString1, pointString2, distance))))
     val resultDf = spark.sql("select * from point1 p1, point2 p2 where ST_Within(p1._c0, p2._c0, "+arg3+")")
     resultDf.show()
 
     return resultDf.count()
   }
 
-  val ST_Contains = (queryRectangle:String, pointString:String) => {
+  def ST_Contains (queryRectangle:String, pointString:String) : Boolean = {
 
     // split the rectangle string into individual coordinates
-    val recArr = queryRectangle.split(",");
+    var recArr = queryRectangle.split(",")
 
     // first point
-    val x1 = recArr(0).toDouble;
-    val y1 = recArr(1).toDouble;
+    var x1 = recArr(0).toDouble
+    var y1 = recArr(1).toDouble
 
     // second point
-    val x2 = recArr(2).toDouble;
-    val y2 = recArr(3).toDouble;
+    var x2 = recArr(2).toDouble
+    var y2 = recArr(3).toDouble
 
     // point under test
-    val point = pointString.split(",")
-    val pointx = point(0).toDouble;
-    val pointy = point(1).toDouble;
+    var point = pointString.split(",")
+    var pointx = point(0).toDouble
+    var pointy = point(1).toDouble
 
-    x1 <= pointx && pointx <= x2 &&
-           y1 <= pointy && pointy <= y2;
+    if (x1 <= pointx && pointx <= x2 && y1 <= pointy && pointy <= y2)
+      return true
+    else
+      return false
   }
 
-  val ST_Within = (pointString1:String, pointString2:String, maxDistance:Double) => {
+  def ST_Within (pointString1:String, pointString2:String, distance:Double): Boolean = {
     // split the rectangle string into individual coordinates
-    val recArr = pointString1.split(",");
+    var recArr = pointString1.split(",");
 
     // point1
-    val point1 = pointString1.split(",")
-    val x1 = point1(0).toDouble;
-    val y1 = point1(1).toDouble;
+    var point1 = pointString1.split(",")
+    var x1 = point1(0).toDouble;
+    var y1 = point1(1).toDouble;
 
     // point2
-    val point2 = pointString2.split(",")
-    val x2 = point2(0).toDouble;
-    val y2 = point2(1).toDouble;
+    var point2 = pointString2.split(",")
+    var x2 = point2(0).toDouble;
+    var y2 = point2(1).toDouble;
 
-    // distance formulat √((x2-x1)^2 + (y2-y1)^2)
-    val distanceFromAtoB = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1,2))
+    // distance formula √((x2-x1)^2 + (y2-y1)^2)
+    var distanceFromAtoB = math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1,2))
 
-    distanceFromAtoB <= maxDistance;
+    if (distanceFromAtoB <= distance)
+      return true
+    else
+      return false
   }
 }
